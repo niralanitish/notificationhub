2c61cf2ef9fe21eb8d7b5bb96bc2d18fa6fc5bd7
Nitish Nirala <nnirala110@gmail.com>
2026-06-29 10:26:53 +0530

    Move frontend to static folder

--- src/main/java/com/notificationhub/notificationhub/controller/HomeController.java
+++ src/main/java/com/notificationhub/notificationhub/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    
     public String home() {
         return """
                 🚀 Notification Hub API is Running!
